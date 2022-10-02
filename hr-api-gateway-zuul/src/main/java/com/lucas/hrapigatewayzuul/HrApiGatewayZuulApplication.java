f5c18fefee335a4eec1473d6aceefcb0c4976c6e
Lucas <lucas-50p@hotmail.com>
2022-10-02 18:04:27 -0300

    Configuring Zuul api gateway

---
+++ hr-api-gateway-zuul/src/main/java/com/lucas/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -0,0 +1,24 @@
+package com.lucas.hrapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class HrApiGatewayZuulApplication {
+	
+	
+	/*
+	 * API GATEWAY ZULL: gerenciar e rotear chamadas a partir do nomes dos micro serviços
+	 * Idependente de quantas instâncias exista.
+	 * Então, a partir de um ponto especifico, vai especificar apenas pelo nome e o end point dele
+	 * GATEWAY vai fazer o papel de rotear e encontrar a melhor instância cada micro serviço para atender a requisição
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatewayZuulApplication.class, args);
+	}
+
+}
