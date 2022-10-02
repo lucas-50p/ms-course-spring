3b7f8a66de0695d2daac5827ac63f93971756f77
Lucas <lucas-50p@hotmail.com>
2022-10-01 22:31:23 -0300

    Project hr-api-gateway-zull-created

--- hr-worker/src/main/java/com/lucas/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/lucas/hrworker/HrWorkerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 @SpringBootApplication
 public class HrWorkerApplication {
-
+	
 	/*
 	 * O servidor EUREKA vai tratar de registrar todas as instâncias caso seja mais de uma.
 	 * Quando precisar de um micro serviço, chama o micro serviço apenas pelo nome.
