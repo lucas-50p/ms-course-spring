79d3f0ef39cdd3a2ee49953cf3f202b010e99a3b
Lucas <lucas-50p@hotmail.com>
2022-10-19 10:19:39 -0300

    Configuring hr-user project

--- hr-user/src/main/java/com/lucas/hrconfigserver/HrUserApplication.java
+++ hr-user/src/main/java/com/lucas/hrconfigserver/HrUserApplication.java
@@ -2,9 +2,17 @@ package com.lucas.hrconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrUserApplication {
+	
+	/*
+	 * O servidor EUREKA vai tratar de registrar todas as instâncias caso seja mais de uma.
+	 * Quando precisar de um micro serviço, chama o micro serviço apenas pelo nome.
+	 * Tudo vai ser automático
+	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
